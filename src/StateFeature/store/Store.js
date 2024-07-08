d50ace3be5bc12d1299d80ebc16f2dbcd80c18c5
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2024-07-08 16:51:43 +0600

    redux-toolkit setup for register  info

---
+++ src/StateFeature/store/Store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "../Slice/authSlice";
+
+export const store = configureStore({
+  reducer: {
+    registration: authSlice,
+  },
+});
+
+export default store;
