d76e6f9d009ea05385627f66384c5dd285034ed0
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2024-12-05 00:23:31 +0600

    post commet text and emoji get done

--- src/components/pageComponents/Home/HomeMiddle/CreateComments.jsx
+++ src/components/pageComponents/Home/HomeMiddle/CreateComments.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import avatar from "../../../../../public/postBackgrounds/man.jpg";
 import { MdOutlineEmojiEmotions, MdOutlinePermMedia } from "react-icons/md";
+import EmojiPicker from "emoji-picker-react";
 
 const CreateComments = ({
   userInfo,
@@ -13,6 +14,21 @@ const CreateComments = ({
   setCommentError,
   inputRef,
 }) => {
+  const [emojiPicker, setEmojiPicker] = useState(false);
+  const [cursorPosition, setCursorPosition] = useState();
+  const handleEmojiClick = ({ emoji }, e) => {
+    const ref = inputRef.current;
+    ref.focus();
+    const textStart = commentText.substring(0, ref.selectionStart);
+    const textEnd = commentText.substring(ref.selectionEnd);
+    const newText = textStart + emoji + textEnd;
+    setCommentText(newText);
+    setCursorPosition(textStart.length + emoji.length);
+  };
+  useEffect(() => {
+    inputRef.current.selectionEnd = cursorPosition;
+  }, [cursorPosition, inputRef]);
+
   return (
     <>
       <div className="mt-2 ">
@@ -29,15 +45,29 @@ const CreateComments = ({
               onChange={(e) => setCommentText(e.target.value)}
               value={commentText}
             />
-            <div className="flex gap-2">
-              <MdOutlineEmojiEmotions
-                size={28}
-                className="text-secondary_color cursor-pointer"
-              />
-              <MdOutlinePermMedia
-                size={25}
-                className="text-secondary_color cursor-pointer"
-              />
+            <div className="flex gap-2 relative">
+              <div>
+                <MdOutlineEmojiEmotions
+                  size={28}
+                  className="text-secondary_color cursor-pointer"
+                  onClick={() => setEmojiPicker((prev) => !prev)}
+                />
+                {emojiPicker && (
+                  <div className="absolute -top-[380px] -left-[300px]">
+                    <EmojiPicker
+                      onEmojiClick={handleEmojiClick}
+                      height={370}
+                      width={350}
+                    />
+                  </div>
+                )}
+              </div>
+              <div>
+                <MdOutlinePermMedia
+                  size={25}
+                  className="text-secondary_color cursor-pointer"
+                />
+              </div>
             </div>
           </div>
           <div></div>
